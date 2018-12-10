074c9b2678d740e735249a7a03e99fabbd3805d0
Ricardo <ricardots211@gmail.com>
2018-12-09 21:24:18 -0600

    Actualizacion

---
+++ agenteInteligente/src/Firebase/Paso.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Firebase;
+
+/**
+ *
+ * @author ric21
+ */
+public class Paso {
+    private int id;
+    private String Descripcion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.Descripcion = descripcion;
+    }
+
+    public Paso() {
+    }
+}
